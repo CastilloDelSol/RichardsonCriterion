96532fd1f0eca5809beb77b72b8d6fce7ec4abf8
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2024-05-05 16:40:52 +0200

    Update step_response.ino

--- examples/step_response/step_response.ino
+++ examples/step_response/step_response.ino
@@ -1,34 +1,36 @@
 #include <Arduino.h>
-#include "RichardsonCriterion.h"
+#include <RichardsonCriterion.h>
 
-double pt1(double x) {
+double pt1(double x)
+{
     return 2.0 * (1.0 - exp(-x / 0.5));
 }
 
-void setup() {
-	Serial.begin(9600);
-    while (!Serial) {
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial)
+    {
         ; // Wait for serial port to connect
     }
+	
     double step = 0.1;
     double epsilon = 0.001;
     double t = 0.0;
-    bool converged = false;
 
     Serial.println("x\t\tf(x)\t\tsettled");
     Serial.println("-------------------------------------");
 
-	RichardsonCriterion<double> criterion;
-    while (!converged)
+    RichardsonCriterion<double> richardson(epsilon);
+    while (!richardson.hasConverged())
     {
         double ft = pt1(t);
-        criterion.addDataPoint(ft);
-        converged = criterion.hasConverged(epsilon);
+        richardson.addDataPoint(ft);
         Serial.print(t);
         Serial.print("\t\t");
         Serial.print(ft, 6);
         Serial.print("\t\t");
-        Serial.println(converged ? "true" : "false");
+        Serial.println(richardson.hasConverged(epsilon) ? "true" : "false");
         t += step;
     }
 
@@ -39,5 +41,7 @@ void setup() {
     Serial.println(epsilon);
 }
 
-void loop() {
+void loop()
+{
+	// empty
 }
